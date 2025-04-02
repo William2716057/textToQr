f3eadebb6ee29b7ac17e7253fe5fda2b9978e339
William <willspri@gmail.com>
2025-04-03 12:48:40 +1300

    qr functions added

--- textToQr.py
+++ textToQr.py
@@ -1,6 +1,17 @@
 
 from PIL import Image
 import matplotlib.pyplot as plt
+import qrcode
+
+input = input("Input text: ")
+
+qr = qrcode.QRCode(
+    version=1, #version
+    error_correction=qrcode.constants.ERROR_CORRECT_L, #error correction
+    box_size=10, #size of each box in QR
+    border=4, #border thickness
+    )
+
 
 image = Image.open("image.jpg")
 
